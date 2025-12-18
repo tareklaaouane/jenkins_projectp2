090a9d993804fcc7c48e9a948af417deed4b41c2
tareklaaouane <tlaaouane@gmail.com>
2025-12-18 15:52:29 +0100

    jenkins file DEPLOY

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
