4031dc0e21e0110af8c51a20f30409781042a3f8
tareklaaouane <tlaaouane@gmail.com>
2025-12-18 14:51:16 +0100

    jenkins

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator A = new Calculator();
+        System.out.println(A.add(1, 2));
+        System.out.println(A.add(1, 2));
+    }
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
+    }
+}
